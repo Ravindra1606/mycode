58c049eeb96ce0ee3de83703fedf56cc485761a6
Ravindra1606 <shuklaravi887883@gmail.com>
2023-02-01 12:07:02 +0530

    second commit

--- mycode.py
+++ mycode.py
@@ -1,29 +1,13 @@
-def main():
-    print("Simple and Compound Interest Calculator")
-    print("1. Simple Interest")
-    print("2. Compound Interest")
-    choice = int(input("Enter your choice (1 or 2): "))
-    if choice == 1:
-        p = float(input("Enter the principal amount: "))
-        r = float(input("Enter the rate of interest: "))
-        t = float(input("Enter the time (in years): "))
-        si = (p * r * t) / 100
-        print("Simple Interest:", si)
-    elif choice == 2:
-        p = float(input("Enter the principal amount: "))
-        r = float(input("Enter the rate of interest: "))
-        t = float(input("Enter the time (in years): "))
-        n = int(input("Enter the number of times the interest is compounded in a year: "))
-        ci = p * (pow((1 + (r / n)), (n * t)))
-        print("Compound Interest:", ci)
-    else:
-        print("Invalid Choice")
 
-if __name__ == '__main__':
-    main()
+def simple_interest(principal, rate, time):
+  return (principal * rate * time) / 100
 
+p = 10000
+r = 10
+t = 2
 
-
+interest = simple_interest(p, r, t)
+print("Simple Interest: ", interest)
 
 
 
