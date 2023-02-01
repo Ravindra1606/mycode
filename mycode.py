3f47e6e064206f2ced8034b5f96b9a0f20e9c334
Ravindra1606 <shuklaravi887883@gmail.com>
2023-02-01 12:13:35 +0530

    SECOND COMMIT

--- mycode.py
+++ mycode.py
@@ -9,6 +9,17 @@ t = 2
 interest = simple_interest(p, r, t)
 print("Simple Interest: ", interest)
 
+def compound_interest(principal, rate, time): 
+
+ A = principal * (pow((1 + rate / 100), time)) 
+
+ CI= A - principal 
+
+ print("Compound interest is", CI) 
+
+
+compound_interest(10000,5,2)
+
 
 
 
